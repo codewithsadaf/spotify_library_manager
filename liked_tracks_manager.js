0d0c5a9c706cb7b24299626e24d6c68b5f26ab88
Ignacio Varela <ivar0728@gmail.com>
2023-05-16 13:55:43 -0600

    refactored to use api wrapper to retrieve tracks instead of axios

--- liked_tracks_manager.js
+++ liked_tracks_manager.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const axios = require('axios');
+const Spotify = require('spotify-web-api-node')
 const {ClientCredentials, ResourceOwnerPassword, AuthorizationCode} = require('simple-oauth2');
+const { default: SpotifyWebApi } = require('spotify-web-api-js');
 
 var CLIENT_ID = 'ff4ce56cef1c4c8aaad752b36e6a6af0'
 var CLIENT_SECRET = 'a48b61222b214b1788512d9885dbc39a'
 var REDIRECT_URI = 'http://localhost:3000/callback/'
 var SCOPE = 'user-library-read'
+
+//Create instance of api wrapper
+var spotifyAPI = new Spotify()
 
 //Construct config object
 const config = {
@@ -53,39 +58,58 @@ app.get('/callback', async function(req, res){
     //Request access Token
     try {
         const result = await client.getToken(options);
-        accessToken = result.token.access_token 
+        accessToken = result.token.access_token
+        spotifyAPI.setAccessToken(accessToken) 
     } catch (error) {
         console.error(error.message);
         res.send('Access Token Error')
     }
-
-    //Make API request
-    axiosGetOptions = {
-        headers: {
-            'Authorization' : 'Bearer ' + accessToken
-        }
-    }
     
-    endpoints = [];
-    for (let n = 0; n < 34; n++){
-        offset = n * 50;
-        endpoints.push(`https://api.spotify.com/v1/me/tracks?limit=50&offset=${offset}`)
-    }
-
-    const requests = endpoints.map(url => axios.get(url, axiosGetOptions))
-
-    axios.all(requests).then(responses => {
-        let data = [];
-        responses.forEach(resp => {
-            for (let i = 0;  i < resp.data.items.length; i++){
-                data.push(resp.data.items[i].track.name)
-            }
-        })
-        res.send(data)
+    //use wrapper to retrieve user saved tracks
+    spotifyAPI.getMySavedTracks({
+        limit: 50,
+        offset: 0
+    }).then(data => {
+        console.log(data.body.items.length)
+        res.send(data.body)
     }).catch(error => {
         console.error(error)
     })
+
+    // //Make API request
+    // //build header that includes access token
+    // axiosGetOptions = {
+    //     headers: {
+    //         'Authorization' : 'Bearer ' + accessToken
+    //     }
+    // }
     
+    // //***NEEDS FIX-currently hardcoded to 1700 tracks***
+    // //Generate endpoints
+    // endpoints = [];
+    // for (let n = 0; n < 34; n++){
+    //     offset = n * 50;
+    //     endpoints.push(`https://api.spotify.com/v1/me/tracks?limit=50&offset=${offset}`)
+    // }
+
+    // //Perform get request on all endpoints, return promise, not response
+    // const requests = endpoints.map(url => axios.get(url, axiosGetOptions))
+
+    // //consolidate responses
+    // axios.all(requests).then(responses => {
+    //     let data = [];
+    //     responses.forEach(resp => {
+    //         for (let i = 0;  i < resp.data.items.length; i++){
+    //             data.push(resp.data.items[i].track.name)
+    //         }
+    //     })
+    //     res.send(data)
+    // }).catch(error => {
+    //     console.error(error)
+    // })
+    
+    //Old code used for single get call, no .map()
+    //Keeping just in case I cant generalize .map() way to any # of tracks
 
     // names = [];
     // let n = 0;
@@ -110,7 +134,7 @@ app.get('/callback', async function(req, res){
 
 
 app.get('/my_tracks', async function(req, res){
-})    
+})
 
 app.get('/', function(req, res) {
     res.send(config)
